d80792ee80f8d3728400ff457e615a4f6c6448c6
pdog <pdog@qq.com>
2018-09-18 21:20:12 +0800

    first commit

---
+++ app/src/main/java/android/app/ActivityThread.java
@@ -0,0 +1,6 @@
+package android.app;
+public class ActivityThread {
+    public static Application currentApplication() {
+        return null;
+    }
+}
---
+++ app/src/main/java/android/app/Application.java
@@ -0,0 +1,6 @@
+package android.app;
+
+import android.content.Context;
+
+public class Application extends Context {
+}
